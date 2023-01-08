89f3fd9aa0194a7df2504ef9ffac498d16b06aac
Arun <2021.arun.rajak@ves.ac.in>
2023-01-08 12:08:02 +0530

    Added Dictionary

---
+++ Dictionary_python.py
@@ -0,0 +1,6 @@
+# Dictionary in python are key:value pairs , the keys are unique and case Sensitive
+# ditioanry is denoted with curly braces {key:value,key:value}
+# to access an element of dictionary we use []
+bramhandPhaseII={601:"Rajak",603:"Mohite",604:"Vishwakarma"}
+print("To print all values of Dictionary: ",bramhandPhaseII)
+print("To print a specific value we use keys: eg 601 is ",bramhandPhaseII[601])
